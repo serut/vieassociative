91625797538d154b164219833691f8cf2c0e11a4
serut <l.mieulet@gmail.com>
2013-09-20 10:53:32 +0200

    Template

--- app/views/template/aside/avancement.blade.php
+++ app/views/template/aside/avancement.blade.php
@@ -1,22 +1,6 @@
 <div class="background">
     <h3 class="head">{{Lang::get('aside.avancement_head')}}</h3>
     Deadline : début 2014<br>
-    <span class="justify">Déploiement du site en ligne :</span>
-    <div class="progress progress-striped active">
-    	<div class="bar" style="width: 100%;"></div>
-    </div>
-    <span class="justify">Développement du noyau : </span>
-    <div class="progress progress-striped active">
-    	<div class="bar" style="width: 95%;"></div>
-    </div>
-    <span class="justify">Creation de la video : </span>
-    <div class="progress progress-striped active">
-    	<div class="bar" style="width: 90%;"></div>
-    </div>
-    <span class="justify">Design du logo : </span>
-    <div class="progress progress-striped active">
-    	<div class="bar" style="width: 80%;"></div>
-    </div>
     <span class="justify">Développement du profil associatif : </span>
     <div class="progress progress-striped active">
     	<div class="bar" style="width: 30%;"></div>

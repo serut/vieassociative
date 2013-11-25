bbb7cffe0b030f0e455361365a7b4494baa4f134
serut <l.mieulet@gmail.com>
2013-11-25 16:01:31 +0100

    Fix mail for Heroku

--- app/controllers/EmailController.php
+++ app/controllers/EmailController.php
@@ -6,7 +6,7 @@ class EmailController extends BaseController
 		// data for send the mail to the user
 		$data=array('name'=>$name,'email'=>$email);
 		Mail::send('mail.register', $data, function ($message) use ($data) {
-			$message->subject('Welcome!');
+			$message->subject('Bienvenue sur Vie Associative !');
 			$message->from('noreply@vieassociative.fr', 'Vie Associative');
 			$message->to($data['email']);
 		});

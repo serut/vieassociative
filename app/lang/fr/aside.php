6c78eaf5ecfc0a3fb683fd41b070867ac89d0e33
serut <l.mieulet@gmail.com>
2014-01-10 19:36:22 +0100

    Aside - change text

--- app/lang/fr/aside.php
+++ app/lang/fr/aside.php
@@ -2,8 +2,8 @@
 //	""=>"",
 return array(
 	"add_association_head"=>"Association",
-	"add_association_text"=>"Nous référencons les associations pour leur donner un moyen de communication propre et puissant. Prenez le temps d'ajouter votre association et completer son profil, elle sera bien plus facile à trouver sur Internet",
-	"add_association_btn"=>"Continuer",
+	"add_association_text"=>"Notre but est de référencer les associations et de mettre à leur disposition des outils puissants et simples d'utilisation pour les aider aussi bien au niveau de la communication que de la gestion ! Ajoutez votre association et complétez son profil afin de découvrir toutes les possibilités qui s'offrent à vous !",
+	"add_association_btn"=>"Rejoignez-nous !",
 	"facebook_head"=>"Facebook",
 	"facebook_text"=>"Ce site n'a que peu de contenu. Sans votre soutien, ce projet n'a rien de mieux que ce qui se fait déjà sur la toile. Alors n'hésitez pas à nous partager !",
 	"avancement_head"=>"Avancement",

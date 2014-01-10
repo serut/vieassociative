d5e04ec3df2e2dbda0654da48a9e0f2a5cff8af4
serut <l.mieulet@gmail.com>
2014-01-10 19:41:42 +0100

    Add link to Trello

--- app/views/template/aside/avancement.blade.php
+++ app/views/template/aside/avancement.blade.php
@@ -1,6 +1,7 @@
 <div class="background">
     <h3 class="head">{{Lang::get('aside.avancement_head')}}</h3>
     <span class="justify">Vous pouvez savoir où nous en sommes dans notre travail  
-    <a href="{{URLSubdomain::to('association','/')}}">ici</a></span><br>
-    Deadline : début 2014<br>
+    <a href="{{URLSubdomain::to('association','/')}}">ici</a>
+    ou sur notre page <a href="https://trello.com/b/lFQz2XsM/vie-associative">Trello</a></span><br>
+    Deadline : février-mars 2014<br>
 </div>

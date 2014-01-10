dc64a3ee2176e95079eeab3fb148f5022e995763
serut <l.mieulet@gmail.com>
2014-01-10 08:54:29 +0100

    Change text

--- app/lang/fr/aside.php
+++ app/lang/fr/aside.php
@@ -1,8 +1,8 @@
 <?php 
 //	""=>"",
 return array(
-	"add_association_head"=>"Ajoute ton association",
-	"add_association_text"=>"Dolor sit amet, consectetur adipiscing elit. Nulla in ligula risus. Nullam aliquet ullamcorper arcu, interdum bibendum orci dictum id. In et velit a nunc fermentum suscipit. Phasellus dui libero, sol !",
+	"add_association_head"=>"Association",
+	"add_association_text"=>"Nous référencons les associations pour leur donner un moyen de communication propre et puissant. Prenez le temps d'ajouter votre association et completer son profil, elle sera bien plus facile à trouver sur Internet",
 	"add_association_btn"=>"Continuer",
 	"facebook_head"=>"Facebook",
 	"facebook_text"=>"Ce site n'a que peu de contenu. Sans votre soutien, ce projet n'a rien de mieux que ce qui se fait déjà sur la toile. Alors n'hésitez pas à nous partager !",

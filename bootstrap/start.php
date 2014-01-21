7edc8d8b69a47a98aa214185e35ad7f8668f0655
serut <l.mieulet@gmail.com>
2014-01-21 17:38:38 +0100

    Fix Issue Laravel 4.1 & Env of prod and local

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,8 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 
 $env = $app->detectEnvironment(array(
-	'local' => array('Serut-PC'),
-	'prod' => array('*'),
+	'local' => array('Serut-PCc')
 ));
 
 /*

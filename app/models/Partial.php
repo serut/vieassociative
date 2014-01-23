0355346fd2c5e97c768425826ed780442ef9ebb2
serut <l.mieulet@gmail.com>
2014-01-23 11:52:54 +0100

    Fix query news when no result

--- app/models/Partial.php
+++ app/models/Partial.php
@@ -30,25 +30,28 @@ class Partial extends Eloquent
     	}
     }
     static function getNews($news){
-    	$listNews = array();
-    	foreach ($news as $n) {
-    		$listNews[] = $n->id;
-    	}
-    	$query = DB::select('select *,partial.updated_at as updated_at from partial 
-    			LEFT JOIN partial_title ON partial.partial_type = "PartialTitle" AND partial_title.id = partial.partial_id
-    			LEFT JOIN partial_text ON partial.partial_type = "PartialText" AND partial_text.id = partial.partial_id
-	    		where partial.id_news in ('.implode(',',$listNews).')
-	    		ORDER BY partial.id_news DESC, partial.order ASC');
-    	$result = array();
-    	$id_news = -1;
-    	$i = 0;
-    	foreach ($query as $q) {
-    		if($id_news != $q->id_news){
-    			$i++;
-    			$id_news = $q->id_news;
-    		}
-    		$result[$id_news][] = $q;
-    	}
+        $result = array();
+        if(!$news->isEmpty()){
+
+        	$listNews = array();
+        	foreach ($news as $n) {
+        		$listNews[] = $n->id;
+        	}
+        	$query = DB::select('select *,partial.updated_at as updated_at from partial 
+        			LEFT JOIN partial_title ON partial.partial_type = "PartialTitle" AND partial_title.id = partial.partial_id
+        			LEFT JOIN partial_text ON partial.partial_type = "PartialText" AND partial_text.id = partial.partial_id
+    	    		where partial.id_news in ('.implode(',',$listNews).')
+    	    		ORDER BY partial.id_news DESC, partial.order ASC');
+        	$id_news = -1;
+        	$i = 0;
+        	foreach ($query as $q) {
+        		if($id_news != $q->id_news){
+        			$i++;
+        			$id_news = $q->id_news;
+        		}
+        		$result[$id_news][] = $q;
+        	}
+        }
     	return $result;
     }
 }

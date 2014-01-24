d2504389faab3507e07dbdeb642ffdb0e53988a3
serut <l.mieulet@gmail.com>
2014-01-24 17:26:35 +0100

    W3C

--- app/views/association/wall/generic-head.blade.php
+++ app/views/association/wall/generic-head.blade.php
@@ -1,5 +1,5 @@
 <div class="media social-box">
 	<a href="#" class="pull-left social-users-avatars" style="width: 48px;">
-		<img src="{{$association->getLogo()}}" class="img-circle">
+		<img src="{{$association->getLogo()}}" class="img-circle" alt="">
 	</a>
 	<div class="media-body social-body">

b227ee91084228bb4dcefb85e71101c5ea70fba5
serut <l.mieulet@gmail.com>
2014-01-24 17:27:16 +0100

    Last W3C step

--- app/views/template/main_html_component/head.blade.php
+++ app/views/template/main_html_component/head.blade.php
@@ -5,7 +5,6 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vie Associative</title>
     <meta name="description" content="">
